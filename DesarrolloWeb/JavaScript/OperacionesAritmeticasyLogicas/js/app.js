975faaf89de9aa376e2ba136ddd1cccf37cf8a5c
jmfabbiani <jose.fabbiani@gmail.com>
2018-08-03 17:33:43 -0400

    Estructuras Secuenciales y Operaciones Aritmetcias JS

---
+++ DesarrolloWeb/JavaScript/OperacionesAritmeticasyLogicas/js/app.js
@@ -0,0 +1,7 @@
+function areaTriangulo() {
+  var a=0,b=0,h=0;
+  b=prompt("Ingrese la base(b) del triangulo.");
+  h=prompt("Ingrese la altura (h) del triangulo.");
+  a=(b*h)/2;
+  alert("El área del triangulo es: "+a);
+}

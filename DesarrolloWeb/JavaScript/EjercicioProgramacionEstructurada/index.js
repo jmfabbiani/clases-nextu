8e8c758e360e44d409a37da3a07114ef2af4fa00
jmfabbiani <jose.fabbiani@gmail.com>
2018-08-06 15:23:42 -0400

    Ejercicio Programacion Estructurada

--- DesarrolloWeb/JavaScript/EjercicioProgramacionEstructurada/index.js
+++ DesarrolloWeb/JavaScript/EjercicioProgramacionEstructurada/index.js
@@ -11,13 +11,23 @@ var premio = ""
 var valorTotal = 0
 
 // Algoritmo de calculo
+for (var indice in Pedido.productos) {
+  resultado = resultado + (Pedido.precios[indice] * Pedido.cantidad[indice])
+}
 
+valorTotal = resultado + (resultado * IVA)
+
+if (valorTotal > 10000) {
+  premio = "Tiene derecho a un premio"
+} else {
+  premio = "No tiene derecho a un premio"
+}
 
 
 var button = document.getElementById('calculo')
 button.addEventListener('click', function(){
-
   // Aqui debes poner tu alerta
-  
+  alert("Señor(a): " + Pedido.cliente + " el valor total de su pedido es: " + resultado + " y aplicando el IVA: " + valorTotal +
+                  ", debido al valor de su compra: " + premio)
 
 })

f3c52a26f6b5bdfb03b9a10103d086439bdc4ba1
jmfabbiani <jose.fabbiani@gmail.com>
2018-08-06 14:31:38 -0400

    Ejercicio Programacion Estructurada

---
+++ DesarrolloWeb/JavaScript/EjercicioProgramacionEstructurada/index.js
@@ -0,0 +1,23 @@
+var IVA = 0.16
+var Pedido = {
+  cliente: "Federico Gutierrez",
+  productos: ["Manzanas", "Refrescos", "Azucar", "Sal", "Lechugas frescas"],
+  precios: [500, 4000, 2500, 2000, 5000],
+  cantidad: [20, 10, 3, 3, 10]
+}
+
+var resultado = 0
+var premio = ""
+var valorTotal = 0
+
+// Algoritmo de calculo
+
+
+
+var button = document.getElementById('calculo')
+button.addEventListener('click', function(){
+
+  // Aqui debes poner tu alerta
+  
+
+})

cc7a35de70852b48655d5e3adcc31cc32475e233
Jose Fabbiani <jose.fabbiani@gmail.com>
2018-08-06 23:18:02 -0400

    Cambio

--- DesarrolloWeb/JavaScript/EstructurasCondicionales/js/app.js
+++ DesarrolloWeb/JavaScript/EstructurasCondicionales/js/app.js
@@ -1,3 +1,4 @@
+/*
 var a=0,b=0,opcion=0,r=0;
 a=prompt("Ingrese el valor de a:");
 b=prompt("Ingrese el valor de b");
@@ -8,3 +9,31 @@ if(a>b){
 }
 
 alert("El valor de r: "+r)
+*/
+
+var a=0,b=0,opcion=0,r=0;
+a=prompt("Ingrese el valor de a:");
+b=prompt("Ingrese el valor de b:");
+
+opcion= prompt("Ingrese el numero correspondiente a la operacion que desa ejecutar: \n 1. r = a + b \n 2. r = a - b \n 3. r = a * b \n (*) Si ingresa un valor diferente a estos numeros se ejecutara la operacion r = a / b ");
+
+a = Number(a);
+b = Number(b);
+opcion = Number(opcion);
+
+switch (opcion) {
+  case 1:
+    r = a + b;
+    break;
+  case 2:
+    r = a - b;
+    break;
+  case 3:
+    r = a * b;
+    break;
+  default:
+    r = a / b;
+    break;
+  }
+
+alert("El valor de r: "+r);

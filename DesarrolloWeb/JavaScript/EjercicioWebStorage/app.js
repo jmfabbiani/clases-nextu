056e3c0e8c7cfed849e144e55a12b4ac829b4a1e
jmfabbiani <jose.fabbiani@gmail.com>
2018-09-12 13:53:02 -0400

    Ejercicio Web Storage

--- DesarrolloWeb/JavaScript/EjercicioWebStorage/app.js
+++ DesarrolloWeb/JavaScript/EjercicioWebStorage/app.js
@@ -45,21 +45,26 @@ function consultarUsuario(documento) {
   var usuario = JSON.parse(localStorage.getItem('usuario'))
   if (usuario.numeroDoc === documento) {
     document.getElementById('nombreUsuarioRes').value = usuario.nombreUsuario
+    document.getElementById('nombreComRes').value = usuario.nombreCompleto
+    document.getElementById('correoRes').value = usuario.correo
+  }else{
+   alert('El numero de documento ingresado no corresponde con el usuario alamacenado')
   }
 }
 
 botonRegistro.addEventListener('click', function(e) {
   e.preventDefault()
-
+  guardarUsuario()
 })
 
 botonReserva.addEventListener('click', function(e) {
   e.preventDefault()
-
+  guardarReserva()
 })
 
 inputDocumento.addEventListener('keypress', function(e) {
   if (e.which === 13) {
-
+    var Documento = inputDocumento.value
+    consultarUsuario(Documento)
   }
 })

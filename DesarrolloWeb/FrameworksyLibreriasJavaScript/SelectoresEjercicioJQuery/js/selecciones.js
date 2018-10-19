14dc5b069ec4ed2a8a59f937958a9848b3f0990f
jmfabbiani <jose.fabbiani@gmail.com>
2018-10-19 16:47:07 -0400

    Update DesarrolloWeb/FrameworksyLibreriasJavaScript/SelectoresEjercicioJQuery/js/selecciones.js

--- DesarrolloWeb/FrameworksyLibreriasJavaScript/SelectoresEjercicioJQuery/js/selecciones.js
+++ DesarrolloWeb/FrameworksyLibreriasJavaScript/SelectoresEjercicioJQuery/js/selecciones.js
@@ -1,8 +1,15 @@
 $( document ).ready(function() {
   $("body").click(function(){
     //Realiza las selecciones en este bloque de código
+    $("div h1").css("color","yellow");
+    $("div span").find('i').css("color","yellow");
+    $("#fh5co-main-nav").find(":contains('Home')").css("color","yellow");
+    $("img[src$='2.jpg']+a").find(":contains('15 images')").css("color","yellow");
+    $("div[data-section='about'] div:nth-child(2)").find("h2:contains('Jean Smith') ~ p").css("color","yellow");
+    $("#fh5co-contact div.section-heading p:last-of-type a").css("color","yellow");
+    $("ul.slides cite").css("color","yellow");
+    $(".fh5co-footer-widget p:nth-of-type(2):not(a)").css("color","yellow");
 
 
-    
   });
 });

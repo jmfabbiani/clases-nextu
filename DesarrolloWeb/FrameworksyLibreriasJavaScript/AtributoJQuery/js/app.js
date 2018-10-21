41362e530ce487325b0ca4e257f65f02a2394159
Jose Fabbiani <jose.fabbiani@gmail.com>
2018-10-20 20:25:37 -0400

    modificacion de Atributos y Contenido JQuery

--- DesarrolloWeb/FrameworksyLibreriasJavaScript/AtributoJQuery/js/app.js
+++ DesarrolloWeb/FrameworksyLibreriasJavaScript/AtributoJQuery/js/app.js
@@ -6,19 +6,11 @@ $( document ).ready(function() {
     window.alert("La ruta de la imágen es: "+ ruta);
   });
 
-
   $("#logo").click(function(){
     $("#s2").attr("src","image/s2.jpg");
     $("#s2").attr("width","50%");
     $("#s3").attr("src","image/s3.png");
     $("#s3").attr("width","70%");
-
-    for (var i = 1; i < 4; i++) {
-      var seleccion = "form div:nth-of-type("+i+") input";
-      $(seleccion).attr("id", function(){
-        return "input"+i;
-      });
-    };
 
     $("#l1").attr({
       href: "http://www.google.com",
@@ -27,12 +19,19 @@ $( document ).ready(function() {
 
     $("input").removeAttr("placeholder");
 
+
+    for (var i = 1; i < 4; i++) {
+      var seleccion = "form div:nth-of-type("+i+") input";
+      $(seleccion).attr("id", function(){
+        return "input"+i;
+      });
+    }
+
   });
 
   $("input").click(function(){
     var id = $(this).attr("id");
     window.alert("El id del elemento es es: "+ id);
   });
-
 
 });

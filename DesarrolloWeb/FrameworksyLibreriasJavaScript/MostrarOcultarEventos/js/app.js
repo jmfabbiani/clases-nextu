52625f70caf7f5557dd39ec8506d23c567114db9
jmfabbiani <jose.fabbiani@gmail.com>
2018-10-26 16:31:42 -0400

    Ejercicio Ocultar Mostrar JQuery

---
+++ DesarrolloWeb/FrameworksyLibreriasJavaScript/MostrarOcultarEventos/js/app.js
@@ -0,0 +1,38 @@
+$(function(){
+
+
+  $("#btn-vaca").click(function(){
+
+    $("#vaca").toggle("slow");
+
+  });
+
+
+
+
+  //CODIGO PARA LA CAPTURA
+  //
+  // $("selectorDisparador").evento(function(){
+  //
+  //   $("selectorObjetivo").show();
+  //
+  // });
+  //
+  //
+  // function funcionDefinida(){
+  //   $("selectorObjetivo").show();
+  // };
+  //
+  // $("selectorDisparador").evento(funcionDefinida());
+  //
+
+
+  // $("selectorDisparador").evento(function(){
+  //
+  //   $(this).show();
+  //
+  // });
+
+
+
+})

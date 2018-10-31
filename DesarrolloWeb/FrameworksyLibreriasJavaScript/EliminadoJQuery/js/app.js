219c74daa670d6746b71a93aab154868f866aba7
jmfabbiani <jose.fabbiani@gmail.com>
2018-10-31 14:58:28 -0400

    JQuery Clases

---
+++ DesarrolloWeb/FrameworksyLibreriasJavaScript/EliminadoJQuery/js/app.js
@@ -0,0 +1,12 @@
+$(function(){
+  $(".card").mouseover(function(){
+    $(this).removeClass("blue");
+    $(this).addClass("yellow");
+  });
+  $(".card").mouseout(function(){
+    $(this).removeClass("yellow");
+    $(this).addClass("blue");
+  });
+
+})
+  
